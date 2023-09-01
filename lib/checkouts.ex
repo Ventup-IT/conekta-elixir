55e69e5cae8cdd8eeb2b529f69e3ffc85d726cea
Alejandro Pedraza <ales.pedrazam@gmail.com>
2023-08-31 19:36:04 -0600

    chore(payment link): added functions to get and cancel

--- lib/checkouts.ex
+++ lib/checkouts.ex
@@ -10,4 +10,42 @@ defmodule Conekta.Checkouts do
         {:ok, Poison.decode!(body, as: PaymentLinkResponse)}
     end
   end
+
+  @doc """
+  Get Payment Link
+  [Conekta Documenation](https://developers.conekta.com/reference/getcheckout)
+
+  **Method**: `GET`
+
+  Conekta.Checkouts.get_payment_link(payment_link_id)
+      # => { :ok, %Conekta.PaymentLinkResponse{}}
+  """
+  @spec get_payment_link(String.t()) :: {:ok, any()}
+  def get_payment_link(payment_link_id) do
+    case Client.get_request("checkouts/#{payment_link_id}") do
+      {:ok, content} ->
+        content
+        |> Handler.handle_status_code()
+        |> then(&{:ok, Poison.decode!(&1, as: PaymentLinkResponse)})
+    end
+  end
+
+  @doc """
+  Cancel Payment Link
+  [Conekta Documenation](https://developers.conekta.com/reference/cancelcheckout)
+
+  **Method**: `PUT`
+
+  Conekta.Checkouts.cancel_payment_link(payment_link_id)
+      # => { :ok, %Conekta.PaymentLinkResponse{}}
+  """
+  @spec cancel_payment_link(String.t(), Conekta.PaymentLink.t()) :: {:ok, any()}
+  def cancel_payment_link(payment_link_id, payment_link) do
+    case Client.put_request("checkouts/#{payment_link_id}/cancel", payment_link) do
+      {:ok, content} ->
+        content
+        |> Handler.handle_status_code()
+        |> then(&{:ok, Poison.decode!(&1, as: PaymentLinkResponse)})
+    end
+  end
 end
